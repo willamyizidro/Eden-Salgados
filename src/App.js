02a92057dffa622a96ae3280dcb2c4118d3bf88c
WILLAMY <willamyizidro@hotmail.com>
2025-01-14 22:17:48 -0300

    corrigindo numeros

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import IfoodLogo from "./assets/ifood.png";
 const App = () => {
   const links = [
     { name: "iFood", url: "https://www.ifood.com.br/delivery/guarulhos-sp/eden-salgados-jardim-sao-domingos/6e0a9a76-7d0c-4c45-8575-996251bc53fd", icon: IfoodLogo, className: "ifood" },
-    { name: "Loja 1 - Bairro: São Domingos", url: "https://wa.me/message/3CAZHU4GWLBYI1", icon: faWhatsapp, className: "whatsapp" },
-    { name: "Loja 2 - Bairro: São João - Rua Coqueiral", url: "https://wa.me/message/IXJQU4O4AXUWF1", icon: faWhatsapp, className: "whatsapp" },
-    { name: "Loja 3 - Bairro: São João - Estr. Guarulhos-Nazaré", url: "https://wa.me/qr/DY4M4ZXTO4J5I1 ", icon: faWhatsapp, className: "whatsapp" },
-    { name: "Loja 4 - Bairro: Jd São Domingos ", url: "https://wa.me/message/53OBOPJZ6FR6D1", icon: faWhatsapp, className: "whatsapp" },
+    { name: "Loja 1 - Bairro: São Domingos", url: "https://wa.me/5511999550074", icon: faWhatsapp, className: "whatsapp" },
+    { name: "Loja 2 - Bairro: São João - Rua Coqueiral", url: "https://wa.me/5511942103884", icon: faWhatsapp, className: "whatsapp" },
+    { name: "Loja 3 - Bairro: São João - Estr. Guarulhos-Nazaré", url: "https://wa.me/5511984445583", icon: faWhatsapp, className: "whatsapp" },
+    { name: "Loja 4 - Bairro: Jd São Domingos ", url: "https://wa.me/5511915736728", icon: faWhatsapp, className: "whatsapp" },
     { name: "Instagram", url: "https://www.instagram.com/edensalgadoss/", icon: faInstagram, className: "instagram" },
   ];
 
